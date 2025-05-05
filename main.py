6f86c92603b26c8653e2b60c5fe63326e4d0d593
law101 <lawrencebolu@gmail.com>
2025-05-05 10:26:00 +0100

    AIDA 465: added a new fucntion to view list

--- main.py
+++ main.py
@@ -11,4 +11,11 @@ def get_proxy():
     proxy = response.text.strip()
     return proxy
 
+def get_proxy_list():
+    url = "http://api.ip.data5u.com/dynamic/get.html?order=970505113258629&sep=3"
+    response = requests.get(url)
+    proxy_list = response.text.strip().split('\n')
+    return proxy_list
+
+
 print(get_proxy())
